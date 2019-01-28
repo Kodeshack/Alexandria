587b6785315ffef6414a376b4beaa5af2e05cff2
Robin Thrift <services@robinthrift.com>
2019-01-28 18:03:42 +0100

    refactor(routing): move admin routes to own folder

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,7 +8,7 @@ import (
 	"github.com/gorilla/mux"
 	blackfriday "gopkg.in/russross/blackfriday.v2"
 
-	"./models"
+	"./routes"
 )
 
 var ContentPrefix = "content/"
@@ -30,47 +29,7 @@ func resolveRealFilePath(path string) ([]byte, error) {
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/user", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-
-		if r.Header.Get("content-type") != "application/json" {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		data, err := ioutil.ReadAll(r.Body)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		u := models.CreateUser{}
-		err = json.Unmarshal(data, &u)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		user, err := models.NewUser(u.Email, u.DisplayName, u.Password, u.Admin)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		resJSON, err := user.JSON()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-		w.Write(resJSON)
-	})
+	routes.AdminRoutes(r)
 
 	r.HandleFunc("/{path:[\\w\\d_/-]+}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
---
+++ routes/admin.go
@@ -0,0 +1,55 @@
+package routes
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"../models"
+)
+
+func AdminRoutes(r *mux.Router) {
+	r.HandleFunc("/api/user", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		if r.Header.Get("content-type") != "application/json" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		data, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		u := models.CreateUser{}
+		err = json.Unmarshal(data, &u)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		user, err := models.NewUser(u.Email, u.DisplayName, u.Password, u.Admin)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		resJSON, err := user.JSON()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write(resJSON)
+	})
+}
