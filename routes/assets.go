8509c990fd5440c7d404ed894f824769cde1cbde
Robin Thrift <services@robinthrift.com>
2019-04-26 11:27:12 +0200

    docs(assets): add docs for all exported structs and functions

--- routes/assets.go
+++ routes/assets.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// AssetRoutes sets up all routes for static assets such as CSS and JavaScript files.
 func AssetRoutes(r *mux.Router, config *models.Config) {
 	r.PathPrefix(`/assets/{f:[\d\w]+\.[js|css]}`).Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir(config.AssetPath))))
 }
